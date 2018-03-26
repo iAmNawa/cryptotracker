e4400cb6bdb00607a4f26c8ec673f5d7911766e5
Paul Borawski <paulborawski14@gmail.com>
2018-03-26 10:07:28 -0700

    XHR request repeating over and over, need to fix bug later

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Description = (props) => {
-  return (
-    <div>
-     <h1>Search Word: {props.searchy}</h1>
-     <h1>Description here</h1>
-    </div>
-  )
+class Description extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      description: 'words will be here'
+    }
+  }
+
+
+  makeThatCall() {
+    var request = new XMLHttpRequest();
+    //request.addEventListener("load", letUsGetResults);
+    request.open('GET', `https://en.wikipedia.org/w/api.php?action=opensearch&search=${this.state.description}&format=json&origin=*`);
+    request.responseType = 'json';
+
+    request.onload = function() {
+      this.setState({description: request.response[2]})
+      console.log(request.response)
+    }.bind(this)
+    request.send();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 onChange={this.makeThatCall()}>Search Word: {this.props.searchy}</h1>
+        <h1>Description here</h1>
+      </div>
+    )
+  }
 }
 
 export default Description;
