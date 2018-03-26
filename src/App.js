cebdaa8df63023cad236e93701552f9d4b17c25b
Paul Borawski <paulborawski14@gmail.com>
2018-03-26 09:11:38 -0700

    added SearchBar to containers

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Results from './containers/Results/Results';
+import SearchBar from './containers/SearchBar/SearchBar';
 
 class App extends Component {
   render() {
@@ -12,7 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Results />
+        <SearchBar />
       </div>
     );
   }
