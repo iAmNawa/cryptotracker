8660b04f376415726f86a3299489b74e545152ca
Paul Borawski <paulborawski14@gmail.com>
2018-03-25 21:06:57 -0700

    adding &origin=* at the end of the XHR fixed the origin CORS blocking response

--- src/containers/Results/Results.js
+++ src/containers/Results/Results.js
@@ -3,7 +3,28 @@ import React, { Component } from 'react';
 class Results extends Component {
   constructor(props) {
    super(props);
+   this.state = {
+     info: null
+   }
   }
+
+  testXHR = () => {
+    var request = new XMLHttpRequest();
+    //request.addEventListener("load", letUsGetResults);
+    request.open('GET', 'https://en.wikipedia.org/w/api.php?action=opensearch&search=butterfly&format=json&origin=*');
+    request.responseType = 'json';
+
+    request.onload = function() {
+      this.setState({null: request.response.price})
+      console.log(request.response.price)
+    }.bind(this)
+    request.send();
+  }
+
+  componentDidMount = () => {
+    this.testXHR();
+  }
+
   render() {
     return (
       <div>
