e0500a0e7e9475a56822beadc880586ec8f7e16d
Paul Borawski <paulborawski14@gmail.com>
2018-03-26 09:14:18 -0700

    fixed path error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import SearchBar from './containers/SearchBar/SearchBar';
+import SearchBar from './containers/SearchBar';
 
 class App extends Component {
   render() {
